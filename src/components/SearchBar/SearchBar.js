e30c2270f38a49f87b5b328f80dd5647ba926046
Gabriel Lundgren <glundgren93@gmail.com>
2021-10-21 21:54:26 +0100

    Sanitizing input values

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,14 +1,28 @@
 import "./SearchBar.css";
 import React, { useState } from "react";
 
+function sanitize(string) {
+  const map = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "'": "&#x27;",
+    "/": "&#x2F;",
+  };
+  const reg = /[&<>"'/]/gi;
+  return string.replace(reg, (match) => map[match]);
+}
+
 const SearchBar = ({ onSearch }) => {
   const [term, setTerm] = useState("");
 
   const handleSearch = (value) => {
-    setTerm(value);
+    const sanitizedValue = sanitize(value); // prevent xss
+    setTerm(sanitizedValue);
 
     if (onSearch) {
-      onSearch(value);
+      onSearch(sanitizedValue);
     }
   };
 
